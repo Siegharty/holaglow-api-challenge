210de8d717812d523136ac750b86f4fcede4e707
Siegharty <kcabrera866@gmail.com>
2024-01-04 02:44:13 -0300

    Add unit test simple for getNames resource

---
+++ Holaglow.Test.NamesApi/UnitTest/Controllers/PeopleControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using App.Controllers;
+using App.Data;
+using App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Helpers;
+
+namespace UnitTest.Controllers
+{
+
+    public class PeopleControllerTests
+    {
+
+        [Test]
+        public async Task GetNames()
+        {
+            
+            Mock<IPeopleRepository> _mockPeopleRepository = new Mock<IPeopleRepository>();
+            _mockPeopleRepository
+               .Setup(repo => repo.GetListOfNamesAsync("", ""))
+               .ReturnsAsync(new List<PeopleModel>() { new PeopleModel() { gender = "M", name = "Kevin" } });
+
+            PeopleController _controller = new PeopleController(_mockPeopleRepository.Object);
+
+
+            PeopleValidationParams validationParams = new PeopleValidationParams();
+            var result = await _controller.GetNames(validationParams);
+            var getResultValue = GetObjectResult.GetValue(result);
+
+
+            Assert.That(getResultValue.Count(), Is.EqualTo(1));
+            Assert.IsInstanceOf<ActionResult<IEnumerable<PeopleModel>>>(result);
+
+
+        }
+    }
+}
---
+++ Holaglow.Test.NamesApi/UnitTest/Helpers/GetObjectResult.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Helpers
+{
+    public static class GetObjectResult
+    {
+        public static T GetValue<T>(this ActionResult<T> result)
+        {
+            if (result.Result != null)
+                return (T)((ObjectResult)result.Result).Value;
+            return result.Value;
+        }
+
+    }
+}
