5917ca5e3a96644469da6616e81e47fd98336092
Siegharty <kcabrera866@gmail.com>
2024-01-04 01:28:06 -0300

    Adds NUnit project for unit testing

---
+++ Holaglow.Test.NamesApi/UnitTest/Controllers/PeopleController.test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Controllers
+{
+    public class PeopleController
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetNames()
+        {
+            Assert.Pass();
+        }
+    }
+}
