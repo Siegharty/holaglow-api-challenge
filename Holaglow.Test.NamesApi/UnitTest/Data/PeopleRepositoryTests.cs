d08ede4cfd79d55f489e8e0a84df763c91b568e7
Siegharty <kcabrera866@gmail.com>
2024-01-09 01:52:14 -0300

    Update test cases

--- Holaglow.Test.NamesApi/UnitTest/Data/PeopleRepositoryTests.cs
+++ Holaglow.Test.NamesApi/UnitTest/Data/PeopleRepositoryTests.cs
@@ -1,103 +1,81 @@
-﻿using App;
-using App.Controllers;
-using App.Data;
+﻿using App.Data;
 using App.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnitTest.Helpers;
 
 namespace UnitTest.Data
 {
     public class PeopleRepositoryTests
     {
+        private PeopleRepository _repository;
+
         [SetUp]
         public void Setup()
         {
+            _repository = new PeopleRepository();
         }
 
         [Test]
         public async Task ShouldReturnAFilterGenderMale()
         {
-            Mock<PeopleDataStore> _peopleDataStore = new Mock<PeopleDataStore>();
-            _peopleDataStore.Setup(repo => repo.Peoples);
-
-            PeopleRepository _repository = new PeopleRepository();
-
             PeopleValidationParams peopleParams = new PeopleValidationParams() { Name = "", Gender = "M", Page = 1, Size = 10 };
 
             var (result, metaData) = await _repository.GetListOfNamesAsync(peopleParams);
 
-            Assert.That(result.Count(), Is.EqualTo(23));
-            Assert.IsNotNull(result);
+            Assert.That(result.Count(), Is.EqualTo(10));
+            Assert.Multiple(() =>
+            {
+                foreach (var expected in result)
+                {
+                    Assert.AreEqual("M", expected.gender);
+                }
+            }
+            );
 
         }
 
         [Test]
         public async Task ShouldReturnAFilterGenderFemale()
         {
-            Mock<PeopleDataStore> _peopleDataStore = new Mock<PeopleDataStore>();
-            _peopleDataStore.Setup(repo => repo.Peoples);
-
-            PeopleRepository _repository = new PeopleRepository();
 
             PeopleValidationParams peopleParams = new PeopleValidationParams() { Name = "", Gender = "F", Page = 1, Size = 10 };
 
             var (result, metaData) = await _repository.GetListOfNamesAsync(peopleParams);
 
-            Assert.That(result.Count(), Is.EqualTo(29));
-            Assert.IsNotNull(result);
+            Assert.That(result.Count(), Is.EqualTo(10));
+            Assert.Multiple(() =>
+            {
+                foreach (var expected in result)
+                {
+                    Assert.AreEqual("F", expected.gender);
+                }
+            }
+            );
 
         }
 
         [Test]
         public async Task ShouldReturnAPerson()
         {
-            Mock<PeopleDataStore> _peopleDataStore = new Mock<PeopleDataStore>();
-            _peopleDataStore.Setup(repo => repo.Peoples);
-
-            PeopleRepository _repository = new PeopleRepository();
-
-
-            PeopleValidationParams peopleParams = new PeopleValidationParams() { Name = "", Gender = "F", Page = 1, Size = 10 };
+            PeopleValidationParams peopleParams = new PeopleValidationParams() { Name = "Andrea", Gender = "F", Page = 1, Size = 10 };
 
             var (result, metaData) = await _repository.GetListOfNamesAsync(peopleParams);
 
             Assert.That(result.Count(), Is.EqualTo(1));
-            Assert.AreEqual("Marcelo", result.ElementAt(0).name);
-            Assert.AreEqual("M", result.ElementAt(0).gender);
         }
 
         [Test]
         public async Task ShouldReturnAllPersons()
         {
-            /*Mock<PeopleDataStore> _peopleDataStore = new Mock<PeopleDataStore>();
-            _peopleDataStore.Setup(repo => repo.Peoples);
+            PeopleValidationParams peopleParams = new PeopleValidationParams() { Name = "", Gender = "" };
 
-            PeopleRepository _repository = new PeopleRepository();
+            var (result, metaData) = await _repository.GetListOfNamesAsync(peopleParams);
 
-            var result = await _repository.GetAllNames();
-
-            Assert.That(result.Count(), Is.EqualTo(52));
-            Assert.IsNotNull(result);*/
-
-            Assert.Pass();
+            Assert.That(result.Count(), Is.EqualTo(10));
+            Assert.IsNotNull(result);
         }
 
         [Test]
         public async Task ShouldReturnEmptyPerson()
         {
-            Mock<PeopleDataStore> _peopleDataStore = new Mock<PeopleDataStore>();
-            _peopleDataStore.Setup(repo => repo.Peoples);
-
-            PeopleRepository _repository = new PeopleRepository();
-
-
             PeopleValidationParams peopleParams = new PeopleValidationParams() { Name = "Marcel", Gender = "F", Page = 1, Size = 10 };
 
             var (result, metaData) = await _repository.GetListOfNamesAsync(peopleParams);
